0737868908c8f0fb07eece5700914c34b1747a88
KeviniDoAgency <Kevin.esquivel@idoagency.com>
2023-11-29 03:44:40 -0600

    entitty card

---
+++ src/core/enums/card-type.enum.ts
@@ -0,0 +1,4 @@
+export enum CardTypeEnum {
+  CREDIT = 'credit',
+  DEBIT = 'debit',
+}
---
+++ src/db/entity/card.entity.ts
@@ -0,0 +1,54 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToMany,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { CardTypeEnum } from 'src/core/enums/card-type.enum';
+import {
+  IsBoolean,
+  IsDecimal,
+  IsEnum,
+  IsNotEmpty,
+  IsString,
+} from 'class-validator';
+import { BaseEntity } from './base/base';
+import { Transaction } from './transaction.entity';
+import { Account } from './account.entity';
+
+@Entity('card')
+export class Card extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  cardId: number;
+
+  @IsString()
+  @IsNotEmpty()
+  @Column({ unique: true })
+  cardNumber: string;
+
+  @IsEnum(CardTypeEnum)
+  @IsNotEmpty()
+  @Column({
+    enum: CardTypeEnum,
+    type: 'enum',
+  })
+  cardType: CardTypeEnum;
+
+  @IsString()
+  @IsNotEmpty()
+  @Column()
+  cvv: string;
+
+  @IsBoolean()
+  @Column({
+    default: false,
+  })
+  isActive: boolean;
+
+  @IsDecimal()
+  @IsNotEmpty()
+  @Column({ type: 'decimal' })
+  moneyLimit: number;
+}
